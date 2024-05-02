76943c6f065d8ce8dbf9c6b1775fd1760e41bad3
Stephen Glenn <stephen4glenn@gmail.com>
2024-05-02 16:17:15 +0000

    Fixed Comment issue

--- src/main/java/net/ccts/data/Album.java
+++ src/main/java/net/ccts/data/Album.java
@@ -1,5 +1,10 @@
 package net.ccts.data;
-// Fill later
+/**
+ * Represents an album in a music collection.
+ * Users can create instances of the Album class by providing the name of the album and the name of the artist.
+ * Users can also specify the release date of the album.
+ * Once created, users can retrieve album information and update the release date.
+ */
 public class Album{
   private String name;
   private String artist;
@@ -13,17 +18,19 @@ public Album(String n, String a) {
     this.artist = a;
 }
 
+// Get name
 public String getName() {
   return name;
 }
-
+// Get Artist
 public String getArtist() {
   return artist;
 }
-
+// Get Release Year
 public int getreleaseYear() {
   return releaseYear;
 }
+// Parameter of release year. So the number provided must be between the years 1800 and 9999.
 public void setreleaseYear(int releaseYear) {
     if(releaseYear <1800 || releaseYear > 9999) {
 
@@ -35,11 +42,11 @@ throw new IllegalArgumentException(
     this.releaseYear = releaseYear;
     }
   }    
-
+// Get release Month
 public int getreleaseMonth() {
   return releaseMonth;
 }
-
+// Parameter for the Month selection between 1 & 12.
 public void setreleaseMonth (int releaseMonth) {
      if(releaseMonth < 1 || releaseYear >  12) {
 
@@ -50,9 +57,11 @@ throw new IllegalArgumentException(
     this.releaseMonth = releaseMonth;
     }
 }
+// Get release Day.
 public int getreleaseDay() {
   return releaseDay;
 }
+// Parameter for release Day between 1 & 31
 public void setreleaseDay(int releaseDay) {
      if(releaseDay < 1 || releaseYear >  31) {
 
@@ -64,7 +73,7 @@ throw new IllegalArgumentException(
     this.releaseDay = releaseDay;
     }
 }
-
+// Overriding java givens
 @Override
 public String toString() {
   String info = this.name;
