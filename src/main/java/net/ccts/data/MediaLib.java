defe27e4f292391775c9e77a7586745368a579f0
Dean <deaner1121@gmail.com>
2024-05-13 18:22:24 +0000

    array submission

--- src/main/java/net/ccts/data/MediaLib.java
+++ src/main/java/net/ccts/data/MediaLib.java
@@ -7,17 +7,54 @@ package net.ccts.data;
  */
 public class MediaLib
 {
-  private Book book;
+  private Book[] books = new Book[10];
+  private Album[] albums = new Album[50];
 
-  public void addBook(Book b)
-  {
-    book = b;
+  public Book fetchBook (int index) {
+    if(index < 0 || index >= books.length) {
+      throw new IllegalArgumentException("Index" + index + "is out of bounds.");
+    }
+    return this.books[index];
+  }
+  public Album fetchAlbum (int index) {
+    if (index < 0 || index >= albums.length) {
+      throw new IllegalArgumentException("Index" + index + "is out of bounds.");
+    }
+    return this.albums[index];
+  }
+  public void setBook(int index, Book b) {
+    if(index < 0 || index >= books.length) {
+      throw new IllegalArgumentException("Index" + index + "is out of bounds.");
+    }
+    this.books[index]=b;
+    
+  }
+  public void setAlbum(int index, Album b) {
+    if(index < 0 || index >= albums.length) {
+      throw new IllegalArgumentException("Index" + index + "is out of bounds.");
+    }
+    this.albums[index]=b;
+  }
+public Book removeBook (int index) {
+    if(index < 0 || index >= books.length) {
+      throw new IllegalArgumentException("Index" + index + "is out of bounds.");
+    }
+    Book temp =  this.books[index];
+    this.books[index] = null;
+    return temp;
+  }
+  public Album removeAlbum (int index) {
+    if (index < 0 || index >= albums.length) {
+      throw new IllegalArgumentException("Index" + index + "is out of bounds.");
+    }
+    Album temp = this.albums[index];
+    this.albums[index] = null;
+    return temp;
   }
 
   public String toString() 
   {
     String info = "";
-    
     return info;
   }
 }
